7008efe8e952e4f36138407477486422b69f006d
odanielchristopher] <o.dani.br@gmail.com>
2025-03-12 16:07:42 -0300

    lint (api): config eslint

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -21,5 +21,18 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'no-console': 'warn',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        'selector': 'interface',
+        'format': ['PascalCase'],
+        'custom': {
+          'regex': '^I[A-Z]',
+          'match': true
+        }
+      }
+    ]
   },
 };

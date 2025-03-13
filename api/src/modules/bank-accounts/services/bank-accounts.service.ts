7d72aea724d664ea6e8b921fe9e4c342d8a071a1
odanielchristopher] <o.dani.br@gmail.com>
2025-03-13 08:25:03 -0300

    feat (api): creating bank accounts module

---
+++ api/src/modules/bank-accounts/services/bank-accounts.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@nestjs/common';
+import { BankAccountsRepository } from 'src/shared/database/repositories/bank-accounts.repository';
+import { CreateBankAccountDto } from '../dto/create-bank-account.dto';
+import { UpdateBankAccountDto } from '../dto/update-bank-account.dto';
+import { ValidateBankAccountOwnershipService } from './validate-bank-account-ownership.service';
+
+@Injectable()
+export class BankAccountsService {
+  constructor(
+    private readonly bankAccountRepo: BankAccountsRepository,
+    private readonly validateBankAccountOwnershipService: ValidateBankAccountOwnershipService,
+  ) {}
+
+  findAllByUserId(userId: string) {
+    return this.bankAccountRepo.findMany({ where: { userId } });
+  }
+
+  create(userId: string, createBankAccountDto: CreateBankAccountDto) {
+    const { name, initialBalance, type, color } = createBankAccountDto;
+
+    return this.bankAccountRepo.create({
+      data: {
+        userId,
+        name,
+        initialBalance,
+        type,
+        color,
+      },
+    });
+  }
+
+  async update(
+    userId: string,
+    bankAccountId: string,
+    updateBankAccountDto: UpdateBankAccountDto,
+  ) {
+    await this.validateBankAccountOwnershipService.validate(
+      userId,
+      bankAccountId,
+    );
+
+    const { name, initialBalance, type, color } = updateBankAccountDto;
+
+    return this.bankAccountRepo.update({
+      where: { userId, id: bankAccountId },
+      data: {
+        name,
+        initialBalance,
+        type,
+        color,
+      },
+    });
+  }
+
+  async remove(userId: string, bankAccountId: string) {
+    await this.validateBankAccountOwnershipService.validate(
+      userId,
+      bankAccountId,
+    );
+
+    await this.bankAccountRepo.delete({
+      where: { userId, id: bankAccountId },
+    });
+
+    return null;
+  }
+}

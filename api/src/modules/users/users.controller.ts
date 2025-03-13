9de5a7ab52c1c659f59e400e9b0121e108fa4ddc
odanielchristopher] <o.dani.br@gmail.com>
2025-03-12 21:09:27 -0300

    feat (api): creating users module

---
+++ api/src/modules/users/users.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Req } from '@nestjs/common';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get('me')
+  me(@Req() request: any) {
+    console.log({ meUserId: request.userId });
+
+    return this.usersService.getUserById('asd');
+  }
+}
---
+++ api/src/modules/users/users.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { UsersRepository } from 'src/shared/database/repositories/users.repository';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly usersRepo: UsersRepository) {}
+
+  getUserById(userId: string) {
+    return userId;
+  }
+}

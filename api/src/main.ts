c14028fea790d9332f110956ffac4e0aa85c49d2
odanielchristopher] <o.dani.br@gmail.com>
2025-03-13 10:25:45 -0300

    feat (api): enable cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -8,6 +8,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: '*',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
